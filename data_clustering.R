ef8ea118985fc128d00cbb2bbd328866bb5a711a
o-b-p <s1642155@ed.ac.uk>
2020-10-18 12:26:31 +0100

    # scale variables and cluster again

--- data_clustering.R
+++ data_clustering.R
@@ -11,3 +11,19 @@ soils_distances <- dist(soils[,c(4:19)])
 # execute hierarchial clustering
 soils_cluster <- hclust(soils_distances)
 plot(soils_cluster)
+
+# look at distribution of variables
+# some variables have a low range of variation (soil pH from 4 to 6.7)
+# some have a large range of variation (Calcium from 39 to 2292
+# they need to be standardised
+summary(soils[,c(4:19)])
+
+# scale variables
+soils_scaled <- scale(soils[,c(4:19)])
+
+# re-run distances
+soils_distances <- dist(soils_scaled)
+
+# re-cluster using default method = "complete"
+soils_cluster <- hclust(soils_distances)
+plot(soils_cluster)
