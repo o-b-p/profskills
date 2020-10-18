ae61ad905ed91948faab40a58f38fc91c77680fc
o-b-p <s1642155@ed.ac.uk>
2020-10-18 12:57:44 +0100

    # clustering count/zero-inflated data with vegan

--- data_clustering.R
+++ data_clustering.R
@@ -27,3 +27,51 @@ soils_distances <- dist(soils_scaled)
 # re-cluster using default method = "complete"
 soils_cluster <- hclust(soils_distances)
 plot(soils_cluster)
+
+
+# Exercise 6: Clustering with species composition data ####
+# dealing with skewed / zero-inflated / count data
+
+library(tidyverse)
+
+inga <- read.csv("~/profskills/Supplement_5_KGD.csv",row.names = 1)
+
+# get column classes
+str(inga)
+
+# scale variables
+inga_scaled <- scale(inga)
+
+# get distances
+inga_distances <- dist(inga_scaled)
+
+# cluster
+inga_cluster <- hclust(inga_distances,method="complete")
+
+# plot dendrogram
+plot(inga_cluster)
+
+# Based on our knowledge of this system, we would think that upland and 
+# floodplains house very different species
+# but these results suggest otherwise
+# this may be because we used an inappropriate measure of distance
+
+# Base R does not manage these kinds of data well, but library(vegan) does
+
+library(vegan)
+
+inga_distances <- vegdist(inga)
+inga_cluster <- hclust(inga_distances,method="ward.D2")
+plot(inga_cluster)
+
+# compare soils dendro to inga dendro in same plot window
+par(mfrow=c(1,2))
+plot(soils_cluster); plot(inga_cluster)
+par(mfrow=c(1,1)) #to simply reset your plotting window
+
+# It looks like species do a better job of differentiating habitats than our soils data
+
+# if you look at a map of the sites in the original paper, you will see that the 
+# species composition data show a stronger signal of geography than the soils
+
+# Exercise 7: Principal Component Analyses
